00b8cbb3f16226a61125b0b496888f7af8b4665a
abiec013 <aec1020121@yahoo.com>
2017-09-27 10:28:11 -0400

    this is my new commit

---
+++ PerimeterandAreaclassrooms/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimeterandAreaclassrooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ProgramExits = true;
+
+            while (ProgramExits == true)
+
+            {
+                //prompt user to enter values
+                Console.WriteLine("Welcome to Grand Circus");
+                Console.WriteLine("Please enter width value.");
+
+                double width, length, Area;
+
+
+                width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the value for length.");
+
+           
+                length = double.Parse(Console.ReadLine());
+                Area = length * width;
+
+                Console.WriteLine("The area of this room is {0}", Area);
+
+                double Perimeter;
+                Perimeter = length * 2 + width * 2;
+
+                Console.WriteLine("The  perimeter of this room is {0}", Perimeter);
+
+
+
+
+
+                string userChoice;
+                Console.WriteLine("Do you want to run this again?");
+
+                userChoice = Console.ReadLine();
+
+                if (userChoice == "no" || userChoice == "No")
+                {
+                    ProgramExits = false;
+                }
+
+
+
+
+
+            }
+
+        }
+    }
+}
